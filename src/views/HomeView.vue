5dab0e85b9df43de676ced2f04c96dc6461f0722
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-11 15:05:55 +0800

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,10 @@
 
 <template>
   <main>
-    This is the mainview
+    <div class="w-full m-h-screen flex flex-col gap-4 p-28">
+      <div class="w-[8rem] h-[8rem] bg-red-500 rounded-3xl"></div>
+      <div class="w-[8rem] h-[8rem] bg-red-500 rounded-3xl"></div>
+      <div class="w-[8rem] h-[8rem] bg-red-500 rounded-3xl"></div>
+    </div>
   </main>
 </template>
