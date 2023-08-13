ae01cc1d3b9d08a1b18af66819e54df7d55e2077
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-14 02:52:46 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import gsap from 'gsap'
+import { ref, watch } from 'vue'
+
+const tl = gsap.timeline()
+
+const text = ref<string>('')
+
+const textArray = ref<string[]>([])
 </script>
 
 <template>
-  <main>
-    <div class="w-full m-h-screen flex flex-col gap-4 p-28">
-      <div class="w-[8rem] h-[8rem] bg-red-500 rounded-3xl"></div>
-      <div class="w-[8rem] h-[8rem] bg-red-500 rounded-3xl"></div>
-      <div class="w-[8rem] h-[8rem] bg-red-500 rounded-3xl"></div>
+  <main class="w-screen h-screen flex flex-col justify-center place-items-center gap-8">
+    <ul class="w-full flex justify-center">
+      <span v-for="(text, index) in textArray" :key="index">
+        <li class="text text-4xl font-bold opacity-0">{{ text }}</li>
+      </span>
+    </ul>
+    <div class="flex gap-5">
+      <input v-model="text" type="text" class="border rounded-xl px-3" />
+
+      <button class="h-10 w-[16rem] border rounded-xl bg-black font-bold text-white shadow-xl">
+        Toggle
+      </button>
     </div>
   </main>
 </template>
