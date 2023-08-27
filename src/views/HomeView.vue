d7d9115c67332fc7e4bb2ce96e2ec97dd3315910
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-28 07:06:55 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
 import gsap from 'gsap'
 import { onMounted } from 'vue'
+import SplitType from 'split-type'
 
 const tl = gsap.timeline()
+
+onMounted(() => {
+  const text = new SplitType('#brand')
+  tl.to('.char', {
+    duration: 1,
+    y: 0,
+    stagger: 0.1,
+    ease:"power4"
+  })
+})
 </script>
 
 <template>
-  <main class="w-screen h-screen flex justify-center place-items center place-items-center"></main>
+  <header class="w-screen h-screen flex justify-center place-items-center">
+    <h1 id="brand" class="leading-[4rem] font-extrabold text-7xl bg-clip-content [clip-path:polygon(0_0,_100%_0,100%_100%,0%_100%)]">NEKUSO</h1>
+  </header>
 </template>
+
+<style>
+.char {
+  transform: translateY(100%);
+}
+</style>
