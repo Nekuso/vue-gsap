dbcc621d7823f452dd2e1d766c2f8b9c80178d0c
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-30 10:46:19 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,5 +6,16 @@ const tl = gsap.timeline()
 </script>
 
 <template>
-  <main class="w-screen h-screen flex justify-center place-items center place-items-center"></main>
+  <main class="wrapper w-screen h-screen flex flex-col overflow-hidden">
+    <div class="content w-full h-[50%] bg-black flex">
+      <div class="w-screen h-full bg-purple-700 flex-shrink-0"></div>
+      <div class="w-screen h-full bg-blue-700 flex-shrink-0"></div>
+      <div class="w-screen h-full bg-teal-700 flex-shrink-0"></div>
+      <div class="w-screen h-full bg-orange-700 flex-shrink-0"></div>
+      
+    </div>
+  </main>
 </template>
+
+
+<style></style>
