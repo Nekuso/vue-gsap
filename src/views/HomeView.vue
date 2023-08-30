274a8c7264c4fb1c30800eb8efbe4683370206f5
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-30 14:50:59 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,50 @@
 <script setup lang="ts">
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Lenis from '@studio-freight/lenis'
 import { onMounted } from 'vue'
 
-const tl = gsap.timeline()
+gsap.registerPlugin(ScrollTrigger)
+
+const lenis = new Lenis({
+  // duration: 1
+})
+function raf(time: number) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+onMounted(() => {
+  const sections = gsap.utils.toArray('section')
+  let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '.content',
+      pin: true,
+      scrub: true,
+      end: '+=6000',
+      markers: true
+    }
+  })
+})
 </script>
 
 <template>
-  <main class="wrapper w-screen h-screen flex flex-col overflow-hidden">
-    <div class="content w-full h-[50%] bg-black flex">
-      <div class="w-screen h-full bg-purple-700 flex-shrink-0"></div>
-      <div class="w-screen h-full bg-blue-700 flex-shrink-0"></div>
-      <div class="w-screen h-full bg-teal-700 flex-shrink-0"></div>
-      <div class="w-screen h-full bg-orange-700 flex-shrink-0"></div>
+  <main class="wrapper w-full h-screen">
+    <div class="spacer w-full h-screen bg-gray-700"></div>
+
+    <div class="content w-auto h-screen bg-black flex overflow-hidden">
+      <section class="w-screen h-full bg-purple-700 flex-shrink-0"></section>
+      <section class="w-screen h-full bg-teal-700 flex-shrink-0"></section>
+      <section class="w-screen h-full bg-blue-700 flex-shrink-0"></section>
+      <section class="w-screen h-full bg-orange-700 flex-shrink-0"></section>
+      <section class="w-screen h-full bg-yellow-700 flex-shrink-0"></section>
     </div>
+
+    <div class="spacer w-full h-screen bg-gray-700"></div>
   </main>
 </template>
 
-<style></style>
+<style scoped></style>
