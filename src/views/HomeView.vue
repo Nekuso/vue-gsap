a919f2c0ad95e54b739d09ee09c5399710b090ad
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-31 14:35:51 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,26 @@ import gsap from 'gsap'
 import { onMounted } from 'vue'
 
 const tl = gsap.timeline()
+
+const mouseLeave = () => {
+  console.log('leave')
+}
+const mouseEnter = () => {
+  console.log('Enter')
+}
 </script>
 
 <template>
-  <main class="w-screen h-screen flex justify-center place-items center place-items-center"></main>
+  <main class="w-screen h-screen flex justify-center place-items center place-items-center">
+    <ul class="links flex flex-wrap justify-between place-items-center gap-8 font-black w-[400px]">
+      <li v-on:mouseenter="mouseEnter" v-on:mouseleave="mouseLeave" class="text-4xl link">
+        Linkedin
+      </li>
+      <li class="text-4xl link">Instagram</li>
+      <li class="text-4xl link">Github</li>
+      <li class="text-4xl link">Facebook</li>
+      <li class="text-4xl link">Youtube</li>
+      <li class="text-4xl link">Dribble</li>
+    </ul>
+  </main>
 </template>
