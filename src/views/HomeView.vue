7f135ba2307b98453d759bea11b2f610373987c6
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-01 23:55:51 +0800

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,18 +16,18 @@ function onMouseMove(e: any) {
   const content = e.target.firstChild
   const { top, left, width, height } = container.getBoundingClientRect()
 
-  const halfW = width / 2
-  const halfH = height / 2
-  const mouseX = e.clientX - left
-  const mouseY = e.clientY - top
+  const halfW: number = width / 2
+  const halfH: number = height / 2
+  const mouseX: number = e.clientX - left
+  const mouseY: number = e.clientY - top
 
-  const x = gsap.utils.interpolate(-halfW, halfW, mouseX / width)
-  const y = gsap.utils.interpolate(-halfH, halfH, mouseY / height)
+  const x: number = gsap.utils.interpolate(-halfW, halfW, mouseX / width)
+  const y: number = gsap.utils.interpolate(-halfH, halfH, mouseY / height)
 
   gsap.to(content, {
     x: x,
     y: y,
-    duration: 0.6,
+    duration: .3,
     ease: 'power1',
     overwrite: true
   })
@@ -35,12 +35,11 @@ function onMouseMove(e: any) {
 
 function onLeave(e: any) {
   const content = e.target.firstChild
-
   gsap.to(content, {
     x: 0,
     y: 0,
-    ease: 'power3',
-    duration: 0.6,
+    ease: 'elastic',
+    duration: 1,
     overwrite: true
   })
 }
@@ -54,13 +53,13 @@ onMounted(() => {})
       <li
         v-for="item in items"
         :key="item.id"
-        class="box border w-36 h-36 relative"
+        class="box border w-36 h-36 relative overflow-hidden"
         @mousemove="onMouseMove($event)"
         @mouseleave="onLeave"
       >
-        <h2 class="content text-xl absolute top-2/4 left-1/2 -translate-x-2/4 -translate-y-1/2">
-          {{ item.text }}
-        </h2>
+        <div
+          class="content p-3 rounded-full text-xl absolute top-2/4 left-1/2 -translate-x-2/4 -translate-y-1/2 bg-orange-600"
+        ></div>
       </li>
     </ul>
   </main>
