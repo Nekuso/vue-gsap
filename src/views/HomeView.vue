cf363972f7bae1fb85dd500249ae98264525d2bb
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-04 17:42:19 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
 import gsap from 'gsap'
 import { onMounted, onBeforeUnmount } from 'vue'
-import { RouterLink } from 'vue-router'
 import { Routes } from '@/constants/route-names'
+import router from '@/router'
 
 const tl = gsap.timeline()
 
+const exit = async () => {
+  await tl.to('.overlay', {
+    xPercent: -100,
+    duration: 2,
+    ease: "power2.inOut",
+  })
+  router.push({ name: Routes.SECOND })
+}
+
 onMounted(() => {
-  tl.to
+  tl.to('.overlay', {
+    x: '100vw',
+    xPercent: -200,
+    duration: 2,
+    ease: 'power2.inOut'
+  })
 })
 </script>
 
 <template>
-  <main class="content w-full h-screen bg-blue-700 flex justify-center place-items-center">
-    <RouterLink
-      :to="{ name: Routes.SECOND }"
-      @click=""
-      class="text-xl font-black button w-auto px-11 py-4 bg-white rounded-full"
-    >
+  <main class="content w-full h-screen bg-blue-700 flex justify-center place-items-center relative">
+    <div class="overlay absolute w-full h-full bg-slate-950"></div>
+    <button class="text-xl font-black button w-auto px-11 py-4 bg-white rounded-full" @click="exit">
       EXPLORE
-    </RouterLink>
+    </button>
   </main>
 </template>
