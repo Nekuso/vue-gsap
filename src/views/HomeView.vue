cf349f0a06944f68cc3bbb6f25470c9f3bd45b34
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-07 12:14:53 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,12 @@ import gsap from 'gsap'
 import { onMounted } from 'vue'
 
 const tl = gsap.timeline()
-
 </script>
 
 <template>
-  <main class="w-screen h-screen flex justify-center place-items center place-items-center">
-    
+  <main class="w-full flex flex-col justify-center place-items-center">
+    <div class="w-full h-screen"></div>
+    <div class="content w-full h-screen bg-red-700"></div>
+    <div class="w-full h-screen"></div>
   </main>
 </template>
