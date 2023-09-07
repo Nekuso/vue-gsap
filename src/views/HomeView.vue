7b5b4b04501c8cc09bef70f911324eff723f817a
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-07 14:43:37 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,92 @@
 <script setup lang="ts">
 import gsap from 'gsap'
+import Lenis from '@studio-freight/lenis'
 import { onMounted } from 'vue'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline()
+const lenis = new Lenis()
+
+function raf(time: any) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+onMounted(() => {
+  const textElements = gsap.utils.toArray('.text')
+
+  textElements.forEach((text: any) => {
+    tl.to(text, {
+      scrollTrigger: {
+        trigger: text,
+        start: 'center 80%',
+        end: 'center 20%',
+        scrub: true
+      },
+      backgroundSize: '100%',
+      ease: 'none'
+    })
+  })
+})
 </script>
 
 <template>
-  <main class="w-full flex flex-col justify-center place-items-center">
+  <main class="w-full flex flex-col justify-center place-items-center bg-black">
     <div class="w-full h-screen"></div>
-    <div class="content w-full h-screen bg-red-700"></div>
+    <div class="content w-full h-screen px-36 flex flex-col justify-center place-items-center">
+      <h1 class="text font-black">TRACKXP<span>TRY THIS</span></h1>
+      <h1 class="text font-black">ICHIRAKU<span>RAMEN STORE</span></h1>
+      <h1 class="text font-black">HEYMEAL<span>RECIPES!!</span></h1>
+      <h1 class="text font-black">MARINA TOWN <span>Nothing Fancy</span></h1>
+    </div>
     <div class="w-full h-screen"></div>
   </main>
 </template>
+
+<style>
+.text {
+  font-size: 10vw;
+  letter-spacing: -0.01em;
+  line-height: 100%;
+  margin: 0;
+
+  width: 100%;
+  color: rgb(182, 182, 182, 0.2);
+  background: linear-gradient(to right, #b6b6b6, #b6b6b6) no-repeat;
+  -webkit-background-clip: text;
+  background-clip: text;
+  background-size: 0%;
+  transition: background-size cubic-bezier(0.1, 0.5, 0.5, 1) 0.5s;
+
+  border-bottom: 1px solid #2f2b28;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #8d74c2;
+  color: #0d0d0d;
+
+  clip-path: polygon(0 50%, 100% 50%, 100% 50%, 0 50%);
+  transform-origin: center;
+  transition: all cubic-bezier(0.1, 0.5, 0.5, 1) 0.4s;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.text:hover > span {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+}
+</style>
