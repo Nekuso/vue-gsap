905e8699563a36595dcadf26c2c4f5b27e58c76c
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-08 16:53:42 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import gsap from 'gsap'
 import { onMounted } from 'vue'
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis()
+
+function raf(time: number) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 const tl = gsap.timeline()
 </script>
 
 <template>
-  <main class="w-full h-screen flex flex-col justify-center place-items-center">
-        
-  </main>
+  <main class="w-full h-screen flex flex-col justify-center place-items-center"></main>
 </template>
