33f0eb691d0bd7b2662f60deb655938fd27f1fa1
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-09 15:52:43 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,8 +16,13 @@ const tl = gsap.timeline()
 </script>
 
 <template>
-  <main class="w-full h-screen flex flex-col justify-center place-items-center">
+  <main class="w-full flex flex-col justify-center place-items-center">
+    <div class="spacer w-full h-screen bg-slate-800"></div>
+    <div class="content w-full h-screen bg-slate-700">
 
+      
+    </div>
+    <div class="spacer w-full h-screen bg-slate-800"></div>
   
   </main>
 </template>
