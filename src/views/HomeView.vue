f038b46e8425e9cbc56aed2e2340bb02d7e7edd1
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-10 22:23:31 +0800

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,13 +18,13 @@ const tl = gsap.timeline()
 <template>
   <main class="w-full flex flex-col justify-center place-items-center">
     <div class="spacer w-full h-screen bg-slate-800"></div>
-    <div class="content w-full h-screen bg-slate-700 flex flex-col justify-center place-items-center">
+    <div
+      class="content w-full h-screen bg-slate-700 flex flex-col justify-center place-items-center"
+    >
       <div class="marquee w-full bg-white">
         <h1>Marquee</h1>
       </div>
-
     </div>
     <div class="spacer w-full h-screen bg-slate-800"></div>
-  
   </main>
 </template>
