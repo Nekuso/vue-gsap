a107581746003e9468141e9eff132ac1b5e0f182
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-09-13 12:32:29 +0800

    Init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import gsap from 'gsap'
 import { onMounted } from 'vue'
+import { Application } from '@splinetool/runtime'
 
-const tl = gsap.timeline()
+onMounted(() => {
+
+  const canvas = document.getElementById('canvas3d')
+  const app = new Application(canvas)
+  app.load('https://prod.spline.design/sJO5mgflCrP2Mhjo/scene.splinecode')
+})
 </script>
 
 <template>
   <main class="w-full h-screen flex flex-col justify-center place-items-center">
-        
+    <canvas class="w-full h-screen" id="content"></canvas>
   </main>
 </template>
